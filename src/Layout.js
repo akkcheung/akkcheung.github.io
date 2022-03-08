9e8bba6b7d41e3f77e5653889d6c18baf0a96a2c
Alex Cheung <alexkk.cheung@gmail.com>
2022-03-08 12:21:25 +0800

    minor bug fix for Entry List

--- src/Layout.js
+++ src/Layout.js
@@ -4,7 +4,7 @@ const Layout = {
 	view: function(vnode) {
 		return m("main.layout", [
 			m("nav.menu", [
-				// m(m.route.Link, {href: "/list", "class": "btn"}, "Entry List"),
+				m(m.route.Link, {href: "/list", "class": "btn"}, "Entry List"),
 				m(m.route.Link, {href: "/board", "class": "btn"}, "Tic Tac Toe"),
 				m(m.route.Link, {href: "/bookshop", "class": "btn"}, "Book Shop"),
 				m(m.route.Link, {href: "/stopwatch", "class": "btn"}, "Stop Watch"),
