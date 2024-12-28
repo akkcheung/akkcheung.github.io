c79ab9f126f7f4aa2a0d3d5d83e49b84f431a1eb
Gogs <gogs@fake.local>
2024-12-28 08:47:01 -0500

    Generate song menu from json file

--- dh/app.js
+++ dh/app.js
@@ -1,25 +1,11 @@
 const audio = document.getElementById('audio');
 const lyricsContainer = document.getElementById('lyrics');
+const menu = document.getElementById('menu');
 
 let lyrics=[]
 let currentLine = 0;
 
-audio.addEventListener('timeupdate', () => {
-    if (currentLine < lyrics.length && audio.currentTime > lyrics[currentLine].time) {
-        lyricsContainer.innerText = lyrics[currentLine].text;
-        currentLine++;
-    }
-});
-
-audio.addEventListener('ended', () => {
-    currentLine = 0; // Reset when the song ends
-});
-
-const links = document.querySelectorAll('a')
-
-links.forEach(link => {
-  link.addEventListener('click', handleLinkClick)
-})
+// Functions
 
 function handleLinkClick(event){
   event.preventDefault()
@@ -66,3 +52,82 @@ function parseLRC(lrcText) {
     return lyrics;
 }
 
+function getSongs(){
+  fetch('songs.json')
+    .then(response => response.json())
+    .then(data => {
+       data.songs.forEach( song => {
+         console.log(song.name)
+       })
+    })
+}
+
+function getMenu(){
+  fetch('songs.json')
+    .then(response => response.json())
+    .then(data => {
+
+       let songList = ""
+
+       data.songs.forEach( song => {
+         if (String(song.id)[3] === "1"){
+           if (String(song.id)[0] === "1")
+             songList += `<li class="divider" data-content="廣東話"></li>`
+
+           if (String(song.id)[0] === "2")
+             songList += `<li class="divider" data-content="國語"></li>`
+
+           if (String(song.id)[0] === "3")
+             songList += `<li class="divider" data-content="ENG"></li>`
+         }
+
+         songList += `
+         <li class="menu-item">
+            <a href="#" value="${song.id}">${song.name}</a>
+         </li>
+         `
+       })
+       
+       console.log(songList)
+       // return songList
+       
+       menu.innerHTML = songList
+
+
+    })
+}
+
+// EventListeners
+
+audio.addEventListener('timeupdate', () => {
+    if (currentLine < lyrics.length && audio.currentTime > lyrics[currentLine].time) {
+        lyricsContainer.innerText = lyrics[currentLine].text;
+        currentLine++;
+    }
+});
+
+audio.addEventListener('ended', () => {
+    currentLine = 0; // Reset when the song ends
+});
+
+const links = document.querySelectorAll('a')
+
+
+// getSongs()
+// menu.innerHTML = getMenu()
+
+getMenu()
+
+// links.forEach(link => {
+//   link.addEventListener('click', handleLinkClick)
+// })
+
+document.addEventListener('DOMContentLoaded', () => {
+  menu.addEventListener('click', (event) => {
+
+    if (event.target.tagName.toLowerCase() === 'a'){
+      // console.log(event.target)
+      handleLinkClick(event)
+    }
+  })
+})
