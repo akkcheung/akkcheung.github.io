4e6a2ba1b395b3e8099a778f084dcdf7d24d5d53
akkcheung <alexkk.cheung@gmail.com>
2026-02-07 21:43:03 -0500

    ebook app - select book

--- eb/index.js
+++ eb/index.js
@@ -1,6 +1,8 @@
 
-const book = ePub("book.epub");
-const rendition = book.renderTo("viewer", {
+let book = ePub("book.epub");
+// const book = ePub("CNET_T.epub");
+//
+let rendition = book.renderTo("viewer", {
   manager: "continuous",
   flow: "paginated",
   width: "100%",
@@ -8,6 +10,8 @@ const rendition = book.renderTo("viewer", {
 });
 
 const displayed=rendition.display();
+
+let headerText="太白金星有點煩"
 
 var next = document.getElementById("next");
 next.addEventListener("click", function(){
@@ -22,13 +26,69 @@ prev.addEventListener("click", function(){
 const $toc = document.getElementById("toc-container");
 
 book.loaded.navigation.then(function(nav) {
-    const docfrag = document.createDocumentFragment();
-    const header = document.createElement("header");
+  console.log("renderToc...")
+  renderToc(nav.toc)
+});
 
-    header.textContent = "太白金星有點煩 - 馬伯庸";
-    docfrag.append(header)
 
-    nav.toc.forEach(function(chapter) {
+const trigger = document.getElementById('open-modal')
+const modal = document.getElementById('toc')
+
+trigger.addEventListener('click', () => {
+  modal.setAttribute('open', true);
+});
+ 
+console.log('modal', modal)
+
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.removeAttribute('open');
+  }
+});
+
+const picker = document.getElementById('picker')
+console.log('picker', picker)
+
+picker.addEventListener('change', (event) => {
+  const choice = event.target.value;
+  headerText = event.target.options[event.target.selectedIndex].text
+
+  if (choice !== ""){
+    console.log('choice', choice)
+    console.log('headerText', headerText)
+    
+    if (rendition){
+      rendition.destroy()
+    };
+
+    book = ePub(choice);
+
+    rendition = book.renderTo("viewer", {
+      manager: "continuous",
+      flow: "paginated",
+      width: "100%",
+      height: "100%",
+    });
+
+    rendition.display();
+
+    book.loaded.navigation.then(function(nav) {
+      renderToc(nav.toc)
+    })
+
+  }
+})
+
+function renderToc(toc){
+  $toc.innerHTML = "";
+
+   const docfrag = document.createDocumentFragment();
+   const header = document.createElement("header");
+
+   header.textContent = headerText
+   docfrag.append(header)
+
+   toc.forEach(function(chapter) {
         const item = document.createElement("li");
         const link = document.createElement("a");
 
@@ -48,19 +108,4 @@ book.loaded.navigation.then(function(nav) {
     });
 
     $toc.appendChild(docfrag);
-});
-
-const trigger = document.getElementById('open-modal')
-const modal = document.getElementById('toc')
-
-trigger.addEventListener('click', () => {
-  modal.setAttribute('open', true);
-});
- 
-console.log('modal', modal)
-
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.removeAttribute('open');
-  }
-});
+}
